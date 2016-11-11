8c412f24fc22ecab000d7314e860e9130de54165
adaldo <antonio.adaldo.89@gmail.com>
2016-11-11 13:54:39 +0100

    dummy

--- src/footprints.py
+++ src/footprints.py
@@ -22,59 +22,59 @@ class Footprint(object):
 
     def __call__(self, ps, Rs, pl, Rl):
         """Function that returns the value of the footprint.
-        
+
         Args:
             ps: Position of the sensor.
             Rs: Orientation of the sensor.
             pl: Position of the landmark.
             Rl: Orientation of the landmark.
-            
+
         Returns:
             float: value of the footprint (>=0).
-        
+
         """
         raise NotImplementedError()
 
 
-        
+
     def pos_grad(self, ps, Rs, pl, Rl):
         """Function that returns the gradient of the footprint
         with respect to the position.
-        
+
         Args:
             ps: Position of the sensor.
             Rs: Orientation of the sensor.
             pl: Position of the landmark.
             Rl: Orientation of the landmark.
-            
+
         Returns:
             numpy array: gradient of the footprint
                 with respect to the position.
-        
+
         """
         raise NotImplementedError()
 
-        
+
     def ori_grad(self, ps, Rs, pl, Rl):
         """Function that returns the gradient of the footprint
         with respect to the orientation.
-        
+
         Args:
             ps: Position of the sensor.
             Rs: Orientation of the sensor.
             pl: Position of the landmark.
             Rl: Orientation of the landmark.
-            
+
         Returns:
             numpy array 3-by-3:
                 gradient of the footprint
                 with respect to the orientation.
-        
+
         """
         raise NotImplementedError()
 
 
-        
+
     def contour_plot(
             self,
             ps=np.zeros(3),
@@ -88,7 +88,7 @@ class Footprint(object):
             ):
         """Draw a contour plot of the footprint
         as a function of xy position of the landmark.
-        
+
         Args:
             ps: Position of the sensor.
             Rs: Orientation of the sensor.
@@ -103,7 +103,7 @@ class Footprint(object):
             num_points: number of points generated for the plot (default 100)
             filename: name of the file where the plot is saved
                 (default '[ClassName].pdf')
-        
+
         """
         if Rl_func == None:
             def Rl_func(plx, ply):
@@ -132,7 +132,7 @@ class Footprint(object):
         plt.colorbar(cs)
         plt.grid()
         plt.savefig(filename)
-        
+
 
 
 
@@ -145,7 +145,7 @@ class SphericalFootprint(Footprint):
     def __init__(self, best_distance=1.0, ori_weight=1.0):
         self._BEST_DISTANCE = best_distance
         self._ORI_WEIGHT = ori_weight
-        
+
     def __call__(self, ps, Rs, pl, Rl):
         BD = self._BEST_DISTANCE
         OW = self._ORI_WEIGHT
@@ -153,7 +153,7 @@ class SphericalFootprint(Footprint):
         result += np.linalg.norm(ps+BD*Rs[:,0]-pl)**2
         result += OW*np.linalg.norm(ps+BD*Rl[:,0]-pl)**2
         return result
-        
+
     def pos_grad(self, ps, Rs, pl, Rl):
         BD = self._BEST_DISTANCE
         OW = self._ORI_WEIGHT
@@ -161,7 +161,7 @@ class SphericalFootprint(Footprint):
         result += 2*ps+2*BD*Rs[:,0]-2*pl
         result += OW*(2*ps+2*BD*Rl[:,0]-2*pl)
         return result
-        
+
     def ori_grad(self, ps, Rs, pl, Rl):
         BD = self._BEST_DISTANCE
         OW = self._ORI_WEIGHT
@@ -169,17 +169,17 @@ class SphericalFootprint(Footprint):
         result[:,0] += (2*ps+2*BD*Rs[:,0]-2*pl)*BD
         result[:,0] += (2*ps+2*BD*Rl[:,0]-2*pl)*BD
         return result
-        
-        
+
+
     def contour_plot(self):
         BD = self._BEST_DISTANCE
         xlim = (-0.5*BD, 1.5*BD)
         ylim = (-BD, BD)
         Footprint.contour_plot(self, xlim=xlim, ylim=ylim)
-        
-        
-        
-        
+
+
+
+
 
 
 
@@ -196,7 +196,8 @@ class EggFootprint(Footprint):
         self._FRONT_GAIN = front_gain
         self._REAR_GAIN = rear_gain
         self._FACET_GAIN = facet_gain
-        
+
+
     def __call__(self, ps, Rs, pl, Rl):
         BD = self._BEST_DISTANCE
         FG = self._FRONT_GAIN
@@ -205,13 +206,14 @@ class EggFootprint(Footprint):
         vec = ps+BD*Rs[:,0]-pl
         norm = np.linalg.norm(vec)
         one = (FG+RG)/2*norm**2
-        two = (RG-FG)/2*norm*vec.dot(Rs[:,0])
+        two = (RG-FG)/2*norm*(vec.dot(Rs[:,0]))
         vec = ps+BD*Rl[:,0]-pl
         norm = np.linalg.norm(vec)
         three = (FG+RG)/2*norm**2
         four = (RG-FG)/2*norm*vec.dot(Rl[:,0])
         return one + two + MG*(three + four)
-        
+
+
     def pos_grad(self, ps, Rs, pl, Rl):
         BD = self._BEST_DISTANCE
         FG = self._FRONT_GAIN
@@ -220,42 +222,45 @@ class EggFootprint(Footprint):
         vec = ps+BD*Rs[:,0]-pl
         norm = np.linalg.norm(vec)
         one = (FG+RG)*vec
-        if norm==0:
+        if norm<1e-4:
             two = np.zeros(3)
         else:
-            two = (RG-FG)/2*(vec.dot(Rs[:,0])*vec/norm+norm*Rs[:,0])
+            two = (RG-FG)/2*((vec.dot(Rs[:,0]))*vec/norm+norm*Rs[:,0])
         vec = ps+BD*Rl[:,0]-pl
         norm = np.linalg.norm(vec)
         three = (FG+RG)*vec
-        if norm==0:
+        if norm == 0:
             four = np.zeros(3)
         else:
-            four = (RG-FG)/2*(vec.dot(Rl[:,0])*vec/norm+norm*Rl[:,0])
+            four = (RG-FG)/2*((vec.dot(Rl[:,0]))*vec/norm+norm*Rl[:,0])
         return one + two + MG*(three + four)
-        
+
+
     def ori_grad(self, ps, Rs, pl, Rl):
         BD = self._BEST_DISTANCE
         FG = self._FRONT_GAIN
         RG = self._REAR_GAIN
         vec = ps+BD*Rs[:,0]-pl
         norm = np.linalg.norm(vec)
-        one = (FG+RG)*vec
-        if norm==0:
+        one = BD*(FG+RG)*vec
+        if norm<1e-4:
             two = np.zeros(3)
         else:
-            two = (RG-FG)/2*(vec.dot(Rs[:,0])*vec/norm+norm*Rs[:,0]+norm*vec)
-        x = BD*(one + two)
+            two = (RG-FG)/2*(BD*(vec.dot(Rs[:,0]))*vec/norm + norm*(BD*Rs[:,0]+vec))
+        #x = one + two
+        x = np.zeros(3)
         y = np.zeros(3)
         z = np.zeros(3)
         return x, y, z
-        
+
+
     def contour_plot(self, **kwargs):
         BD = self._BEST_DISTANCE
         xlim = (-BD+BD,4.0*BD+BD)
         ylim = (-BD,BD)
         Footprint.contour_plot(self, xlim=xlim, ylim=ylim, **kwargs)
-        
-        
+
+
 
 
 """Test"""
